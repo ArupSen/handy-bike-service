d67b0337f7d6d2a27dd414f6db5d0c95f19a08a6
Arup Sen <arupsen48@gmail.com>
2014-10-14 09:18:20 +0100

    added all content and pulled in sections from header and footer

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+// -- index.php --
+// the unique contents will go here up to the footer
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<?php // leave in during development and remove once live ?>
+<meta name="robots" content="noindex, nofollow">
+
+<title></title>
+<meta name="description" content="" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href='http://fonts.googleapis.com/css?family=Averia+Sans+Libre:700|Cabin:600' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+</head>
+<body>
+        <!--[if lt IE 8]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+<div id="wrap">
+<header>
+<h1>Handy Bike Service</h1>
+<address>GREGORY HAWRO<br /> <a href="mailto:gregory@handybikeservice.co.uk">gregory@handybikeservice.co.uk</a><br />
+07780501007
+</address>
+</header>
+<div id="contents">
+<h2>Get your bike serviced<br />
+when and where you want it</h2>
+<p>Based in South-East of London, covering close by areas, high quality<br /> service and repairs at very competitive prices.</p>
+<p>Get your bike checked and sorted at your own convenience. It will be<br /> ready when and where you want it, whether it is at your home or office.</p>
+<h3>PRICES INFO:</h3>
+<p>Call-out prices depending on location • FREE safety check<br /> Puncture: £5 labour • Brakes / Gears service: £10</p>
+<h3>BRONZE SERVICE: £30</h3>
+<p>Overall check-up, gears and brakes tuned, chain checked and<br /> lubricated, wheels and tyres inspected, brake pads checked / fitted</p>
+<h3>SILVER SERVICE: £50</h3>
+<p>All the above plus wheels checked and trued (rims and hubs serviced).<br /> Service headset, bottom bracket and drivetrain. Replace all cables<br /> and brake pads as necessary.</p>
+<h3>GOLD SERVICE: £90</h3>
+<p>All the above plus bike stripped to the frame, cleaned and all parts<br /> regreased. This service needs to be performed at my workshop.</p>
+<p>Prices exclude part costs</p>
+<aside>
+Quick<br /> reliable<br /> cost-effective
+</aside>
+</div><!-- end of contents -->
+<div id="footer"> </div>
+</div> <!-- end of wrap -->
+</body>
+</html>
